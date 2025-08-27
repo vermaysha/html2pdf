abc358a80aa30c39f97800383d2138ab63d3e994
Ashary Vermaysha <asharyver13@gmail.com>
2025-08-27 20:01:45 +0700

    refactor(config): :recycle: replace home directory retrieval method
    * Changed the method of obtaining the home directory from a shell command to using the built-in `homedir()` function from the 'node:os' module.
    * This improves code readability and eliminates potential issues with shell command execution.

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,8 @@
-import { $ } from 'bun';
 import { homedir, tmpdir } from 'node:os';
 import { join, resolve } from 'node:path';
 
-const home = (await $`echo ~`.text().catch(() => process.cwd())).trim();
+
+const home = homedir();
 const configFile = `${home}/.html2pdf`;
 const cachePath = resolve(home, '.cache', 'puppeteer');
 const githubRepo = {
